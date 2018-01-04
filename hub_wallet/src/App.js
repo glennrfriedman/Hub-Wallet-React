a60d71cbf4cccaaf2a0dc26bf6a0b9f5fbac5b5d
Glenn Friedman <glennfriedman@Glenns-MBP.fios-router.home>
2018-01-03 20:37:29 -0500

    added pseudo code for sending data to each Coincard to App.js

--- hub_wallet/src/App.js
+++ hub_wallet/src/App.js
@@ -7,11 +7,18 @@ import './App.css';
 import './dist/toolkit-light.min.css';
 
 class App extends Component {
-
+  
   // constructor(props){
   //   super(props)
   // }
 
+  // In order to render statcard for each coin saved - 
+  // create function that accomplishes the following 
+  // pull list of saved-coins from database in back-end
+  // end function with return of ( <Coincard coin=`${coinName}` ) after .map or .filter 
+  // then call can be made for each component for each saved coin loaded 
+  // this will allow for 1 "Coincard" component but will load all necessary Coincards 
+  // drawback - not sure how fast/slow this will be with asynchroty of axios calls 
 
   render() {
     return (
